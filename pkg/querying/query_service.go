9dae055b761682bf4293807e40ae0bcdd2dd7bc6
Lucas Franca <lucas.franca@mercadolivre.com>
2022-01-09 15:03:13 -0300

    create query execute service

--- pkg/querying/query_service.go
+++ pkg/querying/query_service.go
@@ -5,5 +5,29 @@ import "context"
 type QueryService service
 
 func (pkg *QueryService) Execute(ctx context.Context, queryID, formatID *string, value interface{}) ([]byte, error) {
-	return nil, nil
+	queryTemplate, err := pkg.Client.templateData.ByID(ctx, queryID)
+	if err != nil {
+		return nil, err
+	}
+
+	formatTemplate, err := pkg.Client.templateData.ByID(ctx, formatID)
+	if err != nil {
+		return nil, err
+	}
+
+	query, err := pkg.Client.formatterData.Transform(ctx, queryTemplate, value)
+	if err != nil {
+		return nil, err
+	}
+
+	columns, columnTypes, values, err := pkg.Client.queryData.Query(ctx, string(query), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return pkg.Client.formatterData.Transform(ctx, formatTemplate, map[string]interface{}{
+		"Columns":     columns,
+		"ColumnTypes": columnTypes,
+		"Values":      values,
+	})
 }
